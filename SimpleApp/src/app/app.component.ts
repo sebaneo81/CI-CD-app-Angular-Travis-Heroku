414e09ea05246b0998809bac91903d5234cd0717
Pabloin <pablo.inchausti.bf@gmail.com>
2018-10-01 11:14:52 -0300

    ID-#4 HighCharts Integration

--- SimpleApp/src/app/app.component.ts
+++ SimpleApp/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My Sample App';
+
+  options: Object;
+
+  constructor() {
+    // Highcharts Integration
+    this.options = {
+      title : { text : 'Angular 2 high charts example with selection event ' },
+      chart: { zoomType: 'x'},
+      series: [{ data: [20.7, 88.8, 222.4, 154.2, 50, 17, 120], }]
+    };
+
+  }
 }
--- SimpleApp/src/app/app.module.ts
+++ SimpleApp/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ChartModule } from 'angular2-highcharts';
+
+declare var require: any;
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    ChartModule.forRoot(require('highcharts')),
     BrowserModule
   ],
   providers: [],
